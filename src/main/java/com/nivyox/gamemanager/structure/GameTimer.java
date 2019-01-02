e10b8f8bdf422abaa1a694176bff9934b534f56f
Niek <niek@keersop.nl>
2019-01-02 02:11:22 +0100

    made gametimer abstract, for own tick event handlign

--- src/main/java/com/nivyox/gamemanager/structure/GameTimer.java
+++ src/main/java/com/nivyox/gamemanager/structure/GameTimer.java
@@ -2,11 +2,13 @@ package com.nivyox.gamemanager.structure;
 
 import com.nivyox.gamemanager.structure.events.GameTimerTickEvent;
 import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.Bukkit;
 
-public class GameTimer implements Runnable {
+public abstract class GameTimer implements Runnable {
 
     @Getter
+    @Setter
     private long time;
 
     @Getter
@@ -21,7 +23,5 @@ public class GameTimer implements Runnable {
     public void run() {
         GameTimerTickEvent event = new GameTimerTickEvent(this, time);
         Bukkit.getPluginManager().callEvent(event);
-
-
     }
 }
