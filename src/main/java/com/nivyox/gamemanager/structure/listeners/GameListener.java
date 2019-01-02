48cfc10536f37b25cb1194907ecd0219f9ff5270
Niek <niek@keersop.nl>
2019-01-02 01:31:56 +0100

    added a gamelistener for catching events

---
+++ src/main/java/com/nivyox/gamemanager/structure/listeners/GameListener.java
@@ -0,0 +1,17 @@
+package com.nivyox.gamemanager.structure.listeners;
+
+import com.nivyox.gamemanager.structure.StandardGame;
+import com.nivyox.gamemanager.structure.events.GamePlayerAddEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class GameListener implements Listener {
+
+    @EventHandler
+    public void onGamePlayerAddEvent(GamePlayerAddEvent event) {
+        StandardGame game = event.getGame();
+        if (game.getGameSettings().getMaxPlayerCount() < game.getPlayers().size() + 1) {
+            event.setCancelled(true);
+        }
+    }
+}
